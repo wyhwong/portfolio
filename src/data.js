63da5029b68acf604d1a4ac5132b5c0e16a0c0c0
wyhwong <wyhwong@link.cuhk.edu.hk>
2023-08-28 23:54:43 +0800

    Updates on skills and miscellaneous

--- src/data.js
+++ src/data.js
@@ -24,6 +24,9 @@ import DeepLearningAILogo from "./images/deeplearning.ai-logo.png";
 import WesLogo from "./images/wes-logo.png";
 import LIGOLogo from "./images/ligo-logo.png";
 import IFTALogo from "./images/ifta-logo.png";
+import CFALogo from "./images/CFA-logo.png";
+import CULogo from "./images/CU-logo.png";
+import YULogo from "./images/YU-logo.png";
 
 // Images (add your images to the /images directory with the same names)
 import LightTheme from "./images/light.jpg";
@@ -66,101 +69,116 @@ export const skillData = [
   },
   {
     id: 1,
+    skill: <Icon icon="logos:kubernetes" className="display-4" />,
+    name: "Kubernetes",
+  },
+  {
+    id: 2,
     skill: <Icon icon="logos:docker-icon" className="display-4" />,
     name: "Docker",
   },
   {
-    id: 2,
+    id: 3,
     skill: <Icon icon="devicon:jenkins" className="display-4" />,
     name: "Jenkins",
   },
   {
-    id: 3,
+    id: 4,
     skill: <Icon icon="devicon:git" className="display-4" />,
     name: "Git",
   },
   {
-    id: 4,
+    id: 5,
     skill: <Icon icon="devicon:traefikproxy" className="display-4" />,
     name: "Traefik",
   },
   {
-    id: 5,
+    id: 6,
     skill: <Icon icon="logos:nginx" className="display-4" />,
     name: "Nginx",
   },
   {
-    id: 6,
+    id: 7,
     skill: <Icon icon="mdi:ansible" className="display-4" />,
     name: "Ansible",
   },
   {
-    id: 7,
+    id: 8,
     skill: <Icon icon="devicon:pytorch" className="display-4" />,
     name: "PyTorch",
   },
   {
-    id: 8,
+    id: 9,
     skill: <Icon icon="logos:tensorflow" className="display-4" />,
     name: "TensorFlow",
   },
   {
-    id: 9,
+    id: 10,
     skill: <Icon icon="devicon:pandas" className="display-4" />,
     name: "Pandas",
   },
   {
-    id: 10,
+    id: 11,
     skill: <Icon icon="logos:seaborn-icon" className="display-4" />,
     name: "Seaborn",
   },
   {
-    id: 11,
+    id: 12,
     skill: <Icon icon="devicon:scikitlearn" className="display-4" />,
     name: "scikit-learn",
   },
   {
-    id: 12,
+    id: 13,
     skill: <Icon icon="logos:python" className="display-4" />,
     name: "Python",
   },
   {
-    id: 13,
+    id: 14,
     skill: <Icon icon="vscode-icons:file-type-cpp3" className="display-4" />,
     name: "C++",
   },
   {
-    id: 14,
+    id: 15,
     skill: <Icon icon="devicon:c" className="display-4" />,
     name: "C",
   },
   {
-    id: 15,
+    id: 16,
     skill: <Icon icon="devicon:go" className="display-4" />,
     name: "Golang",
   },
   {
-    id: 16,
+    id: 17,
     skill: <Icon icon="logos:javascript" className="display-4" />,
     name: "JavaScript",
   },
   {
-    id: 17,
+    id: 18,
     skill: <Icon icon="logos:r-lang" className="display-4" />,
     name: "R",
   },
   {
-    id: 18,
+    id: 19,
     skill: <Icon icon="mdi:sql-query" className="display-4" />,
     name: "SQL",
   },
   {
-    id: 19,
+    id: 20,
     skill: <Icon icon="cib:latex" className="display-4" />,
     name: "LaTeX",
   },
   {
-    id: 20,
+    id: 21,
+    skill: <Icon icon="logos:metabase" className="display-4" />,
+    name: "Metabase",
+  },
+  {
+    id: 22,
+    skill: <Icon icon="logos:redis" className="display-4" />,
+    name: "Redis",
+  },
+  {
+    id: 23,
     skill: <Icon icon="devicon:blender" className="display-4" />,
     name: "Blender",
   },
@@ -279,6 +297,26 @@ export const miscellaneousData = [
     name: "Data Analytics Professional Certificate",
     organization: "Google Career Certificates | Couresra",
     logo: GoogleLogo
+  },
+  {
+    id: 7,
+    name: "Coursework: Financial Markets & Narrative Economics",
+    organization: "Yale University | Couresra",
+    logo: YULogo
+  },
+  {
+    id: 8,
+    name: "Data Science for Investment Professionals Specialization",
+    organization: "CFA Institute | Couresra",
+    logo: CFALogo,
+    duration: "In Progress",
+  },
+  {
+    id: 9,
+    name: "Financial Engineering and Risk Management Specialization",
+    organization: "Columbia University | Couresra",
+    logo: CULogo,
+    duration: "In Progress",
   },
 ];
 
